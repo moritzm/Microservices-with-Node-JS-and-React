2421d6e993e5880667c98bfcc05d8be8a902d990
Moritz <moritz.moldenhauer@gmail.com>
2022-04-30 12:40:35 +0200

    Soem code formatting

--- section_2/mini-microservice-boilerplate/blog/client/src/PostList.js
+++ section_2/mini-microservice-boilerplate/blog/client/src/PostList.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
+    const res = await axios.get('http://localhost:4000/posts');
 
     setPosts(res.data);
   };
@@ -18,7 +18,7 @@ const PostList = () => {
     return (
       <div
         className="card"
-        style={{ width: "30%", marginBottom: "20px" }}
+        style={{ width: '30%', marginBottom: '20px' }}
         key={post.id}
       >
         <div className="card-body">
